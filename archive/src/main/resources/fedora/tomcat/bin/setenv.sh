e24482059b747d388fe84788a4f2a1e358853136
Jan Schnasse <schnasse@gmx.de>
2012-11-14 10:18:16 +0100

    Tomcat tweak

---
+++ archive/src/main/resources/fedora/tomcat/bin/setenv.sh
@@ -0,0 +1,12 @@
+CATALINA_OPTS=" \
+-Xms1536m \
+-Xmx1536m \
+-XX:NewSize=256m \
+-XX:MaxNewSize=256m \
+-XX:PermSize=256m \
+-XX:MaxPermSize=256m \
+-server \
+-Djava.awt.headless=true \
+-Dorg.apache.jasper.runtime.BodyContentImpl.LIMIT_BUFFER=true"
+
+export CATALINA_OPTS
