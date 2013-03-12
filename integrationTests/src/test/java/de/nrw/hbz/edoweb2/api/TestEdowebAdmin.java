de9e327922bf64b42b88a9060324a8c70f649480
Jan Schnasse <schnasse@gmx.de>
2013-03-12 12:30:55 +0100

    Test for EdowebAdmin

---
+++ integrationTests/src/test/java/de/nrw/hbz/edoweb2/api/TestEdowebAdmin.java
@@ -0,0 +1,38 @@
+package de.nrw.hbz.edoweb2.api;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestEdowebAdmin
+{
+	Properties properties;
+
+	@Before
+	public void setUp()
+	{
+		try
+		{
+			properties = new Properties();
+			properties.load(getClass().getResourceAsStream("/test.properties"));
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void testIndex()
+	{
+
+	}
+}
