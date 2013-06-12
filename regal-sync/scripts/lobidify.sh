1f4cbedbe43f32c46c8e12228b54cd2c3739cbc4
Jan Schnasse <schnasse@gmx.de>
2013-06-12 11:14:24 +0200

    lobidfy example as bash script

---
+++ regal-sync/scripts/lobidify.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+source pidlist.sh
+
+function index()
+{
+type=$1
+user=$2
+password=$3
+for i in `pidlist $type`
+do
+curl -u ${user}:${password} -XPOST http://localhost/utils/lobidify/$i
+done
+}
+
+
+index $1 $2 $3
