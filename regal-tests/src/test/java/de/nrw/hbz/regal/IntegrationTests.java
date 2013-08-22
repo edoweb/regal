a2a3e33b0f0f29ef9c53e8f56dd803c1da146358
Jan Schnasse <schnasse@gmx.de>
2013-08-22 13:35:26 +0200

    sync tests are in submodules

--- regal-tests/src/test/java/de/nrw/hbz/regal/IntegrationTests.java
+++ regal-tests/src/test/java/de/nrw/hbz/regal/IntegrationTests.java
@@ -2,6 +2,7 @@ package de.nrw.hbz.regal;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 import de.nrw.hbz.regal.api.TestResource;
 import de.nrw.hbz.regal.api.TestUpdateResource;
@@ -9,17 +10,14 @@ import de.nrw.hbz.regal.api.helper.TestActions;
 import de.nrw.hbz.regal.fedora.FedoraFacadeTest;
 import de.nrw.hbz.regal.sync.TestDigitoolDownloader;
 import de.nrw.hbz.regal.sync.TestDippDownloader;
-import de.nrw.hbz.regal.sync.TestEdoweb2Fedora;
 import de.nrw.hbz.regal.sync.TestEdowebDigitalEntityBuilder;
-import de.nrw.hbz.regal.sync.TestOpusDownloader;
 
 @SuppressWarnings("javadoc")
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TestResource.class, TestUpdateResource.class,
+@SuiteClasses({ TestResource.class, TestUpdateResource.class,
 	TestActions.class, FedoraFacadeTest.class,
 	TestDigitoolDownloader.class, TestDippDownloader.class,
-	TestEdoweb2Fedora.class, TestEdowebDigitalEntityBuilder.class,
-	TestOpusDownloader.class })
+	TestEdowebDigitalEntityBuilder.class })
 public class IntegrationTests {
 
 }
