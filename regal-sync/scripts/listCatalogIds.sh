e577dd61df3cdb6e385c74d9ee8d2d39109bc5f6
Jan Schnasse <schnasse@gmx.de>
2013-09-19 09:00:12 +0200

    use server variable

--- regal-sync/scripts/listCatalogIds.sh
+++ regal-sync/scripts/listCatalogIds.sh
@@ -2,7 +2,7 @@
 type=$1
 server=$2
 pidlist=`./list.sh $type $server`
-for i in $pidlist;do TT=`curl -s http://orthos.hbz-nrw.de/fedora/objects/$i/datastreams/DC/content|grep -o "HT[^<]*\|TT[^<]*"`; echo $i,$TT;done >tmp
+for i in $pidlist;do TT=`curl -s http://${server}/fedora/objects/$i/datastreams/DC/content|grep -o "HT[^<]*\|TT[^<]*"`; echo $i,$TT;done >tmp
 
 sort tmp |uniq
 rm tmp
