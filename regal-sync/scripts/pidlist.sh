16a583434397c0fcfcbf8fe4a447e7198ce20812
Jan Schnasse <schnasse@gmx.de>
2013-10-21 10:07:14 +0200

    resource listing new address resource?type=type

--- regal-sync/scripts/pidlist.sh
+++ regal-sync/scripts/pidlist.sh
@@ -14,7 +14,7 @@ function pidlist()
 {
 type=$1
 server=$2
-curl -XGET http://$server/$type/ 2> /dev/null| sed s/"\,"/"\n"/g | sed s/"\"\([^\"]*\)".*/"\1"/ | tail -n +2
+curl -XGET http://$server/resource?type=$type 2> /dev/null| sed s/"\,"/"\n"/g | sed s/"\"\([^\"]*\)".*/"\1"/ | tail -n +2
 echo
 }
 
