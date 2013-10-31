0375a5732d187ec6655f8d15cc90abdf90e550e7
Jan Schnasse <schnasse@gmx.de>
2013-10-31 10:18:50 +0100

    script for urn providing

---
+++ regal-sync/scripts/addUrn.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+pidfile=$1
+server=$2
+snid=$3
+passwd=$4
+
+for i in `cat $pidfile`
+do
+namespace=`echo $i|grep -o "^[^:]*"`
+id=`echo $i|grep -o "[0-9]*$"`
+
+echo "Add URN for id=$id,namespace=$namespace,snid=$snid"
+echo
+curl -uadmin:$passwd -XPOST "api.edoweb-rlp.de/utils/addUrn?id=$id&namespace=$namespace&snid=$snid"
+echo
+echo
+done
