5389fd2b1de9b56b44421ac5193ce64f7080dd97
Jan Schnasse <schnasse@gmx.de>
2014-02-27 16:27:20 +0100

    bash scripts slightly reworked

--- regal-sync/scripts/index.sh
+++ regal-sync/scripts/index.sh
@@ -1,18 +1,5 @@
 #! /bin/bash
 
-source pidlist.sh
-
-function index()
-{
-type=$1
-user=$2
-password=$3
-server=$4
-for i in `pidlist $type $server repo 0 10000`
-do
-curl -s -u ${user}:${password} -XPOST "http://$server/utils/index/$i?type=$type";echo
-done
-}
-
+source functions.sh
 
 index $1 $2 $3 $4
--- regal-sync/scripts/lobidify.sh
+++ regal-sync/scripts/lobidify.sh
@@ -1,18 +1,5 @@
 #! /bin/bash
 
-source pidlist.sh
-
-function index()
-{
-type=$1
-user=$2
-password=$3
-server=$4
-for i in `pidlist $type $server repo`
-do
-curl -s -u ${user}:${password} -XPOST http://localhost/utils/lobidify/$i;echo
-done
-}
-
+source functions.sh
 
 index $1 $2 $3 $4
